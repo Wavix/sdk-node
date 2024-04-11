e24b1f0e19d59c65b512d798b12ad74310716d5e
Mike K <mike.k@wavix.com>
2024-04-11 13:56:17 +0500

    Fix call service

--- examples/wavix.ts
+++ examples/wavix.ts
@@ -1,2 +1,4 @@
 import Wavix from "../src/index"
-export const wavix = new Wavix({ appid: process.env.APPID })
+export const wavix = new Wavix({
+  appid: "97a277d969b04a2a9354f69b40accbfc05ca5ca1237d4d5491aa48e2bca50d60"
+})
